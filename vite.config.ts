78daafb93f407d0762998f654d918ec2e829a014
Wouter Hisschemöller <wouter@electricbrains.nl>
2022-11-17 20:28:59 +0100

    Icons updated.

--- vite.config.ts
+++ vite.config.ts
@@ -12,22 +12,22 @@ export default defineConfig({
       },
       includeAssets: ['struck-icon.svg', 'struck-icon-180.png'],
       manifest: {
-        name: 'Struck',
-        short_name: 'Struck',
-        start_url: '/struck/index.html',
-        scope: '/struck/',
+        name: 'Calder',
+        short_name: 'Calder',
+        start_url: '/calder/index.html',
+        scope: '/calder/',
         description: 'Percussive action with 3D physics.',
         theme_color: '#3778AF',
         display: 'standalone',
         orientation: 'portrait',
         icons: [
           {
-            src: 'struck-icon-192.png',
+            src: 'calder-icon-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'struck-icon-512.png',
+            src: 'calder-icon-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
